bf6ac3bb42d20660e4f99b5d60b023ec58022da2
方浩 <254941378@qq.com>
2016-03-10 22:15:17 +0800

    修改设置界面

--- ChiHuoBao/app/src/main/java/com/example/dell/chihuobao/appwidget/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.dell.chihuobao.appwidget;
-
-/**
- * Created by fanghao on 2016/3/6.
- */
-public class Test {
-}
--- ChiHuoBao/app/src/main/java/com/example/dell/chihuobao/fragment/SettingFragment.java
+++ ChiHuoBao/app/src/main/java/com/example/dell/chihuobao/fragment/SettingFragment.java
@@ -5,10 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.dell.chihuobao.R;
-import com.example.dell.chihuobao.appwidget.SettingsItemView;
 
 
 public class SettingFragment extends Fragment {
@@ -16,13 +14,13 @@ public class SettingFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		View view=inflater.inflate(R.layout.setting, container, false);
-        SettingsItemView settings_connect_manager= (SettingsItemView) view.findViewById(R.id.settings_connect_manager);
+      /*  SettingsItemView settings_connect_manager= (SettingsItemView) view.findViewById(R.id.settings_connect_manager);
         settings_connect_manager.setOnSettingClickListener(new SettingsItemView.settingClickListener() {
             @Override
             public void rightClick() {
                 Toast.makeText(getActivity(), "a", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 		return view;
 
 	}
