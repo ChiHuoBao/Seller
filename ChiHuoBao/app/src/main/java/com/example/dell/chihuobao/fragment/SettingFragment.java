b088a3ac2c367ad11d35cad586d69c82eca32577
方浩 <254941378@qq.com>
2016-03-11 09:21:30 +0800

    界面修改

--- ChiHuoBao/app/src/main/java/com/example/dell/chihuobao/fragment/SettingFragment.java
+++ ChiHuoBao/app/src/main/java/com/example/dell/chihuobao/fragment/SettingFragment.java
@@ -1,12 +1,15 @@
 package com.example.dell.chihuobao.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.dell.chihuobao.R;
+import com.example.dell.chihuobao.activity.FeedBackActivity;
 
 
 public class SettingFragment extends Fragment {
@@ -14,6 +17,15 @@ public class SettingFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		View view=inflater.inflate(R.layout.setting, container, false);
+        TextView help = (TextView) view.findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), FeedBackActivity.class);
+                startActivity(intent);
+            }
+        });
       /*  SettingsItemView settings_connect_manager= (SettingsItemView) view.findViewById(R.id.settings_connect_manager);
         settings_connect_manager.setOnSettingClickListener(new SettingsItemView.settingClickListener() {
             @Override
